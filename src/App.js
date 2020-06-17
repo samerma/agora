d0a370ed387addc5d17bd1949864710f2d043eec
Samer <samahajna@gmail.com>
2020-06-17 15:34:13 +0300

    a

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import { observer } from 'mobx-react'
+
+@observer
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
